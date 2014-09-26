a2982c6b93a5b209ee1944e18edd42e788f139ab
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2014-09-26 15:25:19 +0100

    Create tools.js

---
+++ CrushTheCrown/tools.js
@@ -0,0 +1,8 @@
+Object.prototype.getKeyByValue = function( value ) {
+    for( var prop in this ) {
+        if( this.hasOwnProperty( prop ) ) {
+             if( this[ prop ] === value )
+                 return prop;
+        }
+    }
+}
